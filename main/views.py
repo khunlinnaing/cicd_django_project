d97e65a0512ea026dae3ca26e157eda5ae30e4b1
khunlinnaing <khunlinnaingoo90@gmail.com>
2025-10-19 18:34:06 +0630

    pudate

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 from .models import Item
 
+
+@require_GET
 def index(request):
     items = Item.objects.all()
     return render(request, 'index.html', {'items': items})
