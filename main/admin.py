21e504d784be1a793a4ec2cf07bd4595b79a0220
khunlinnaing <khunlinnaingoo90@gmail.com>
2025-10-20 15:33:28 +0630

    update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from main.models import *
+from main.models import Post
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display= ['title', 'content']
